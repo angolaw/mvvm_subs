7d7fc18d7ce03d94c93641ec44fea9a3f6501a97
Willian S <wsapcf@gmail.com>
2022-02-14 11:18:10 -0300

    bind recyclerView and adapter

--- app/src/main/java/com/fourdeux/subs/MainActivity.kt
+++ app/src/main/java/com/fourdeux/subs/MainActivity.kt
@@ -6,15 +6,19 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fourdeux.subs.data.model.Subscriber
 import com.fourdeux.subs.data.repository.SubscriberRepository
 import com.fourdeux.subs.databinding.ActivityMainBinding
 import com.fourdeux.subs.infra.SubscriberDatabase
+import com.fourdeux.subs.presentation.MyRecyckerViewAdapter
 import com.fourdeux.subs.presentation.SubscriberViewModel
 import com.fourdeux.subs.presentation.SubscriberViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var  binding: ActivityMainBinding
     private lateinit var  subscriberViewModel: SubscriberViewModel
+    private lateinit var adapter: MyRecyckerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -32,6 +36,24 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        //init recycler
+        initRecyclerView()
+
         //setContentView(R.layout.activity_main)
+    }
+    private fun listItemClicked(subscriber: Subscriber){
+        Toast.makeText(this, "Selecionado ${subscriber.name}",Toast.LENGTH_LONG).show()
+    }
+    private fun initRecyclerView(){
+        binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MyRecyckerViewAdapter { selectedItem: Subscriber -> listItemClicked(selectedItem) }
+        binding.subscriberRecyclerView.adapter = adapter
+        displaySubscribersList()
+    }
+    private fun displaySubscribersList(){
+        subscriberViewModel.getSavedSubscribers().observe(this, Observer {
+            adapter.setList(it)
+            adapter.notifyDataSetChanged()
+        })
     }
 }
